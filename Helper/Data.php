ac60333322e8baed639af262b5774ff6ded4ad9b
gitpol <infortistheme.s@gmail.com>
2017-10-19 20:15:59 +0200

    Initial upload of files

---
+++ Helper/Data.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Company\Simple\Helper;
+
+use Magento\Framework\App\Helper\AbstractHelper;
+
+class Data extends AbstractHelper
+{
+    /**
+     * Test method
+     *
+     * @return string
+     */
+    public function doSomething()
+    {
+        return "Do something.";
+    }
+}
